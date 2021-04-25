5e324adfd188c7a7a6397dc7b185a27ce97733a0
Kamil Jarząb <jarzbkamil@gmail.com>
2021-04-25 22:30:26 +0200

    Added repeating background

---
+++ Assets/Scripts/Background/BackgroundRepeat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRepeat : MonoBehaviour
+{
+    public float backgroundSpeed;
+    
+    private Vector3 _initialPosition;
+    [SerializeField] private BoxCollider2D _boxCollider2D;
+    void Start()
+    {
+        _initialPosition = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        TranslateBackground();
+        RepeatBackground();
+    }
+
+    public void TranslateBackground()
+    {
+        transform.Translate(0, backgroundSpeed * Time.deltaTime, 0);
+    }
+
+    public void RepeatBackground()
+    {
+        if ((_initialPosition.y + transform.position.y) >= _boxCollider2D.size.y / 2)
+        {
+            transform.position = _initialPosition;
+        }
+    }
+}
