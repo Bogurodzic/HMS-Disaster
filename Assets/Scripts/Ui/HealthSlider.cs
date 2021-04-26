31a3fec0b062cd7f108f68332c43ea3bbafe1576
Kamil Jarząb <jarzbkamil@gmail.com>
2021-04-26 23:51:57 +0200

    Added healthbar

---
+++ Assets/Scripts/Ui/HealthSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSlider : MonoBehaviour
+{
+
+    [SerializeField] private Slider _slider;
+    [SerializeField] private ShipHitpoints _shipHitpoints;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float newSliderValue = (float)_shipHitpoints.GetCurrentHitpoints() / 100;
+        _slider.value = newSliderValue;
+    }
+}
