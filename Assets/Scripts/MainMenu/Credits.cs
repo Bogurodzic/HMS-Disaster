13c3db4c29ea6bb56b6a898196f6e6d13618ade7
Kamil Jarząb <jarzbkamil@gmail.com>
2021-04-27 01:39:11 +0200

    Added credits

---
+++ Assets/Scripts/MainMenu/Credits.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _creditsPanel;
+    private bool _isActive = false;
+
+    public void ToggleCreditPanel()
+    {
+        _isActive = !_isActive;
+        _creditsPanel.SetActive(_isActive);
+    }
+}
