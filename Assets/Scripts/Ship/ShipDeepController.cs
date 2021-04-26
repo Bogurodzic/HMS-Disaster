86f0a4691b9fcfc89174920d317fdd5e1728a5f2
Kamil Jarząb <jarzbkamil@gmail.com>
2021-04-26 17:51:38 +0200

    Added ship controller

---
+++ Assets/Scripts/Ship/ShipDeepController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipDeepController : MonoBehaviour
+{
+    private float _timer = 0;
+    private int _deepMeter;
+    void Update()
+    {
+        GoDeeper();
+    }
+    
+    private void GoDeeper()
+    {
+        _timer += Time.deltaTime;
+        if(_timer >= 1)
+        {
+            _deepMeter += 1;
+            _timer = 0;
+        }
+    }
+
+    public int GetCurrentDeep()
+    {
+        return _deepMeter;
+    }
+}
---
+++ Assets/Scripts/Ui/DeepText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeepText : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    [SerializeField] private ShipDeepController _shipDeepController;
+
+    void Start()
+    {
+        ReloadText();
+    }
+
+    void Update()
+    {
+        ReloadText();
+    }
+
+    private void ReloadText()
+    {
+        _text.text = _shipDeepController.GetCurrentDeep() + "ft";
+    }
+}
