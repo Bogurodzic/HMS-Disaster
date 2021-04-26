ee6f84a935a7ee9abadd25bebae5e79718119864
Kamil Jarząb <jarzbkamil@gmail.com>
2021-04-26 18:50:16 +0200

    Improved helm minigame ship and rock spawning logic

--- Assets/Scripts/MiniGames/Helm/Ship.cs
+++ Assets/Scripts/MiniGames/Helm/Ship.cs
@@ -2,15 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Ship : MonoBehaviour
 {
     [SerializeField] private HelmMiniGame _helmMiniGame;
     [SerializeField] private GameObject[] _spawningPoints;
+    [SerializeField] private GameObject _rock;
+    
     private int _currentPositionIndex = 1;
     void Start()
     {
-        
+        _currentPositionIndex = Random.Range(0, 2);
+        RenderShip();
+        _rock.transform.position = new Vector3(_spawningPoints[_currentPositionIndex].transform.position.x,
+            _rock.transform.position.y, _rock.transform.position.z);
     }
     
     void Update()
@@ -20,12 +26,12 @@ public class Ship : MonoBehaviour
             if (_currentPositionIndex > 0)
             {
                 _currentPositionIndex = _currentPositionIndex - 1;
-                transform.position = new Vector3(_spawningPoints[_currentPositionIndex].transform.position.x, transform.position.y, transform.position.z);
+                RenderShip();
             }
             else
             {
                 _currentPositionIndex = 2;
-                transform.position = new Vector3(_spawningPoints[_currentPositionIndex].transform.position.x, transform.position.y, transform.position.z);
+                RenderShip();
             }
         }
         
@@ -34,16 +40,20 @@ public class Ship : MonoBehaviour
             if (_currentPositionIndex < 2)
             {
                 _currentPositionIndex = _currentPositionIndex + 1;
-                transform.position = new Vector3(_spawningPoints[_currentPositionIndex].transform.position.x, transform.position.y, transform.position.z);
+                RenderShip();
             }
             else
             {
                 _currentPositionIndex = 0;
-                transform.position = new Vector3(_spawningPoints[_currentPositionIndex].transform.position.x, transform.position.y, transform.position.z);
+                RenderShip();
             }
         }
     }
 
+    private void RenderShip()
+    {
+        transform.position = new Vector3(_spawningPoints[_currentPositionIndex].transform.position.x, transform.position.y, transform.position.z);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Rock"))
